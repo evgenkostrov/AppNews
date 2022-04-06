271099a4cfe9f8b1b2cce14cdb4d227878cf78ce
evgenkostrov <evgenkostrov@gmail.com>
2022-04-07 02:24:34 +0300

    fragments

---
+++ app/src/main/java/com/ko/appnews/ui/fragment/SimpleFragment.kt
@@ -0,0 +1,17 @@
+package com.ko.appnews.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ko.appnews.R
+
+
+class SimpleFragment : Fragment(R.layout.fragment_simple) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
